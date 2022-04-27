80f57f6237d29504cbce6376c795868908bbb560
dyabk <dzmitry.yabkevich@outlook.com>
2022-04-27 04:49:52 -0400

    Stages accidentally unstaged changes. Created a utility module logger.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(config.MONGO_URL)
+  .then(() => {
+    logger.info("Connected to MongoDB.");
+  })
+  .catch((error) => {
+    logger.error("Encountered an error connecting to MongoDB: ", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+module.exports = app;
