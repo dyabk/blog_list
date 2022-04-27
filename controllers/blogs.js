c91b077b1cccc186b0d7267374c977ab647efe0e
dyabk <dzmitry.yabkevich@outlook.com>
2022-04-27 19:43:02 -0400

    Updated the router object for the app to accept GET and POST API requests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,5 @@
 const blogsRouter = require("express").Router();
-const Blog = require("./models/blog");
+const Blog = require("../models/blog");
 
 blogsRouter.get("/", (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -7,12 +7,33 @@ blogsRouter.get("/", (request, response) => {
   });
 });
 
-blogsRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
+blogsRouter.post("/", (request, response, next) => {
+  const body = request.body;
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
   });
+
+  blog
+    .save()
+    .then((savedBlog) => {
+      response.status(201).json(savedBlog.toJSON());
+    })
+    .catch((error) => next(error));
 });
 
 module.exports = blogsRouter;
