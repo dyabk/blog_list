de36146ea32cf14feb40e9f8dda00f167e785e4d
dyabk <dzmitry.yabkevich@outlook.com>
2022-04-27 04:44:59 -0400

    Modified app.js. Saved the mongoose models into blog.js

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = "mongodb://localhost/bloglist";
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
 
 app.get("/api/blogs", (request, response) => {
   Blog.find({}).then((blogs) => {
