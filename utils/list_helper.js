3ed69a77e8b2193f2168aa8e5874902213f5f7a5
dyabk <dzmitry.yabkevich@outlook.com>
2022-04-28 04:12:06 -0400

    Added the test for the total count of likes under the posted blogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const totalSum = (sum, item) => {
+    return sum + item;
+  };
+
+  return array.length === 0 ? 0 : blogs.reduce(totalSum, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
