88d59d1f0f9b76c8f4b597b7067d7a151080aa2b
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-02 05:04:31 -0400

    Imported and used the mongoose unique validator package

--- models/user.js
+++ models/user.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     minlength: 5,
     required: true,
+    unique: true,
   },
   name: String,
   passwordHash: {
