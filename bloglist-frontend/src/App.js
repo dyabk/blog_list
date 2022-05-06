6cb43f4203e1ecf0a2b54af89316a06876c33b5a
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-06 01:10:44 -0400

    Added the message to show the users if they're logged in

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -65,6 +65,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
