b47aad2de2fd89047897f3916898373f84122f41
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-14 12:56:23 -0400

    Wrote more Cypress tests for the frontend

--- bloglist-frontend/cypress/integration/bloglist_app.spec.js
+++ bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -72,5 +72,68 @@ describe("Bloglist app", function () {
       cy.contains("like").click();
       cy.contains("Likes: 1");
     });
+
+    it("The user who created a blog can delete it", function () {
+      cy.createBlog({
+        title: "test title",
+        author: "test author",
+        url: "test url",
+      });
+      cy.contains("view").click();
+      cy.contains("delete").click();
+      cy.get("html").should("not.contain", "test title");
+    });
+
+    it("Only the user who created a blog can delete it", function () {
+      cy.createBlog({
+        title: "test title",
+        author: "test author",
+        url: "test url",
+      });
+      cy.contains("logout").click();
+
+      const user = {
+        username: "santiago",
+        name: "Santiago",
+        password: "ProgressGuaranteed622",
+      };
+      cy.request("POST", "http://localhost:3003/api/users", user);
+
+      cy.login({ username: "santiago", password: "ProgressGuaranteed622" });
+      cy.get("html").should("not.contain", "test title");
+      cy.contains("view").click();
+      cy.contains("delete").should("not.exist");
+    });
+
+    it("Blogs are ordered according to likes", function () {
+      cy.createBlog({
+        title: "test title1",
+        author: "test author1",
+        url: "test url1",
+        likes: 8,
+      });
+      cy.createBlog({
+        title: "test title2",
+        author: "test author2",
+        url: "test url2",
+        likes: 2,
+      });
+      cy.createBlog({
+        title: "test title3",
+        author: "test author3",
+        url: "test url3",
+        likes: 22,
+      });
+
+      cy.get(".button-view")
+        .click({ multiple: true })
+        .then(() =>
+          cy.get(".blog").then((blogs) => {
+            cy.wrap(blogs[0]).contains("test title3");
+            cy.wrap(blogs[1]).contains("test title1");
+            cy.wrap(blogs[2]).contains("test title2");
+          })
+        );
+    });
   });
 });
